e41992708ab69198ae9d84bdc85585992d2668e1
nickmazurenko <surprisedsunrise@gmail.com>
2023-03-24 20:00:35 +0200

    change image input

--- src/components/defaults/Inputs/ImageInput.tsx
+++ src/components/defaults/Inputs/ImageInput.tsx
@@ -1,55 +1,106 @@
-import React, { useState } from "react";
+import React from "react";
 import Dropzone from "react-dropzone";
 import Image from "next/image";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlinePlus, AiOutlineCamera, AiOutlineClose } from "react-icons/ai";
 import { ChangeEventHandler } from "react";
 
-export type Props = {
+type InputProps = {
   id: string;
+  number?: number;
+  key?: number;
+  index?: number;
+  value: string[];
   className?: string;
   onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
 };
 
-function ImageInput(props: Props) {
-  const [images, setImages] = useState([]);
+type DropZoneProps = {
+  id: string;
+  number: number;
+  index: number;
+  value: string[];
+  onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+};
 
+function ImageDropZone({ onChange, id, value, index, number }: DropZoneProps) {
   const handleDrop = (acceptedFiles: File[]) => {
-    setImages(images.concat(acceptedFiles));
-    props.onChange({
-      target: { id: props.id, value: images.concat(acceptedFiles) },
+    onChange({
+      target: { id: id, value: value.concat(acceptedFiles) },
     });
   };
+  return (
+    <Dropzone maxFiles={number} onDrop={handleDrop}>
+      {({ getRootProps, getInputProps }) => (
+        <div
+          className="flex flex-col gap-2 text-gray-400 text-sm items-center justify-center w-28 h-28 bg-gray-700 rounded-md"
+          {...getRootProps()}
+        >
+          <input {...getInputProps()} />
+          {index === 0 ? (
+            <>
+              <p className="text-center p-2 z-10">Drop files here or click</p>
+              <AiOutlinePlus className="z-0 absolute text-gray-600" size={50} />
+            </>
+          ) : (
+            <AiOutlineCamera className="z-0 absolute text-gray-600" size={50} />
+          )}
+        </div>
+      )}
+    </Dropzone>
+  );
+}
 
+function ImageInput({
+  id,
+  value,
+  className,
+  onChange,
+  number = 3,
+}: InputProps) {
   const renderImages = () => {
-    return images.map((image) => (
-      <Image
-        width={100}
-        height={100}
-        key={image.name}
-        src={URL.createObjectURL(image)}
-        alt={image.name}
-      />
+    return value.map((image, idx) => (
+      <div className="relative text-transparent hover:text-red-700 " key={idx}>
+        <AiOutlineClose
+          onClick={() => {
+            console.log(idx);
+            onChange({
+              target: {
+                id: id,
+                value: [...value.slice(0, idx), ...value.slice(idx + 1)],
+              },
+            });
+          }}
+          size={30}
+          className="absolute right-0 top-0"
+        />
+        <Image
+          width={100}
+          className={idx === 0 ? "border-4 border-blue-700" : ""}
+          height={100}
+          src={URL.createObjectURL(image)}
+          alt={image.name}
+        />
+      </div>
     ));
   };
 
   return (
     <div
-      {...props}
-      className={`cursor-pointer flex flex-wrap items-center justify-center gap-2 w-full ${props.className}`}
+      className={`cursor-pointer flex flex-wrap items-center justify-center gap-2 w-full ${className}`}
     >
       {renderImages()}
-      <Dropzone onDrop={handleDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <div
-            className="flex flex-col gap-2 text-gray-400 text-sm items-center justify-center w-28 h-28 bg-gray-700 rounded-md"
-            {...getRootProps()}
-          >
-            <input {...getInputProps()} />
-            <p className="text-center p-2 z-10">Drop files here or click</p>
-            <AiOutlinePlus className="z-0 absolute text-gray-600" size={50} />
-          </div>
-        )}
-      </Dropzone>
+      {Array(number - value.length)
+        .fill()
+        .map((_, index) => (
+          <ImageDropZone
+            key={index}
+            index={index}
+            value={value}
+            id={id}
+            number={number}
+            onChange={onChange}
+          />
+        ))}
     </div>
   );
 }

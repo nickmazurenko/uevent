91fb689ef70b4eff6ce27c511749bc98b96e4feb
nickmazurenko <surprisedsunrise@gmail.com>
2023-04-06 19:56:31 +0300

    update retrieve all, add serialization

--- src/lib/events/EventService.ts
+++ src/lib/events/EventService.ts
@@ -1,36 +1,112 @@
-import {Organization} from '@prisma/client';
+import { Organization } from "@prisma/client";
+import { Event } from "@prisma/client";
+import prisma from "@/lib/prisma";
 
 export interface ICloudinaryImage {
-  url: string,
-  public_id: string,
-  signature: string,
+  url: string;
+  public_id: string;
+  signature: string;
+}
+
+export interface RetrieveAllOptions {
+  organizationId?: string;
+  tags?: string[];
+  quantity?: number;
+  sortBy: string;
+  sortOrder: string;
+  filter?: Filter;
+}
+
+export enum Filter {
+  today = "today",
+  upcoming = "upcoming",
+  new = "new",
+  specials = "specials",
 }
 
 export default class EventService {
-  static async create (owner: Organization, name: string, description: string, startAt: string, duration: number, images: ICloudinaryImage[], tickets: number, cost: {amount: number, currency: string}, location: JSON, tags: string[]) {
+  static async create(
+    owner: Organization,
+    name: string,
+    description: string,
+    startAt: Date,
+    duration: number,
+    images: ICloudinaryImage[],
+    tickets: number,
+    cost: { amount: number; currency: string },
+    location: JSON,
+    tags: string[]
+  ) {
     const newEvent = await prisma.event.create({
       data: {
         name,
         description,
         images: images.map((image) => image.url),
         tickets,
-        start_at: startAt,
+        start_at: new Date(startAt),
         duration,
+        // @ts-ignore
         cost,
+        // @ts-ignore
         location,
         tags,
         image_ids: images.map((image) => image.public_id),
         image_signatures: images.map((image) => image.signature),
         organizationId: owner.id,
-      }
+      },
     });
     return newEvent;
   }
 
-  static async retrieveAll(organizationId ?: string, tags?: string[]) {
+  static async retrieveAll(
+    options: RetrieveAllOptions = {
+      sortBy: "created_at",
+      sortOrder: "asc",
+    }
+  ): Promise<Event[]> {
+    const { organizationId, tags, quantity, sortBy, sortOrder, filter } =
+      options;
+    const now = new Date();
+    const todayStart = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate()
+    );
+    const todayEnd = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate() + 1
+    );
+
+    let where: any = {
+      organizationId,
+      tags,
+    };
+
+    switch (filter) {
+      case Filter.upcoming:
+        where.start_at = { gte: now };
+        break;
+      case Filter.new:
+        where.created_at = { gte: todayStart };
+        break;
+      case Filter.today:
+        where.start_at = { gte: todayStart, lt: todayEnd };
+    }
+
     const events = await prisma.event.findMany({
-      where: { organizationId, tags}
+      take: quantity,
+      where,
+      include: { attendees: true },
+      orderBy: { [sortBy]: sortOrder },
     });
-    return events;
+
+    const serializedEvents = events.map((event) => ({
+      ...event,
+      start_at: event.start_at.toISOString(),
+      created_at: event.created_at.toISOString(),
+    }));
+    // @ts-ignore
+    return serializedEvents;
   }
 }
